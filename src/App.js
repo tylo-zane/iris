436e319251f15bb0eb4b35c475d548ec34dc3249
tylo-zane <43626511+tylo-zane@users.noreply.github.com>
2022-12-12 21:15:11 -0800

    created app state for loading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      loading: true
+    };
+  }
+
+  render() {
+    if (this.state.loading === true) {
+      return (
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Welcome
+            </p>
+          </header>
+      )
+    } else {
+      return (
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Welcome
+            </p>
+          </header>
+      )
+    }
+  }
 }
 
 export default App;
